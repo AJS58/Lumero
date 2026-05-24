013a2405f00852e9333318937e71c13dd526f274
AJS58 <anthony.sinfield23@gmail.com>
2026-05-25 00:44:21 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# Lumero v0.8 FULL BUILD
+
+This is the full interactive GitHub Pages / PWA prototype.
+
+Upload the CONTENTS of this folder to the root of the Lumero GitHub repository.
+
+Root should contain:
+- index.html
+- reset.html
+- manifest.json
+- service-worker.js
+- version.json
+- .nojekyll
+- assets/lumero-icon.svg
+
+After upload, open:
+https://ajs58.github.io/Lumero/reset.html
+
+Then:
+https://ajs58.github.io/Lumero/index.html?v=0.8
---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='lumero-v0-8-full';
+const ASSETS=['./','./index.html?v=0.8','./manifest.json','./assets/lumero-icon.svg','./version.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html?v=0.8'))));});
