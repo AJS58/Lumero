72fa3161ddcfa273368980659e83c9f159fb5ee9
AJS58 <anthony.sinfield23@gmail.com>
2026-05-25 00:39:37 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='lumero-v0-6-0';
-const ASSETS=['./','./index.html?v=0.6','./manifest.json','./assets/lumero-icon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html?v=0.6'))));});
